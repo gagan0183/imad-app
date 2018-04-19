5fa7a8d02f317157026097c6c7a98b54b0d6b458
Gagan <g.deepsingh1@gmail.com>
2018-04-19 23:47:53 +0530

    added xmlhttprequest

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+button.onclick = function () {
+  var request = new XMLHttpRequest();
+
+  request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE) {
+      if(request.status === 200) {
+        var counter = request.responseText;
+        var span = document.getElementById('count');
+        span.innerHTML = counter.toString();
+      }
+    }
+  };
+
+  request.open('GET', 'http://localhost:8080/counter', true);
+  request.send(null);
+};
